a77dd9af4c8ac67e7be7d65de200300f493c461a
Andrew Sansom <qthequartermasterman@gmail.com>
2019-01-18 08:51:41 -0600

    Initial commit

---
+++ venv/main.py
@@ -0,0 +1,352 @@
+# import libraries
+import urllib2
+from bs4 import BeautifulSoup
+import urlparse
+import re
+
+alreadyGeneratedCourses = {}
+
+
+class Course:
+    prereqs = []
+    coreqs = []
+
+    def _get_dom_from_webpage(self):
+        # type: () -> BeautifulSoup
+        # We will scape the data from the webpage, so as to make it accesible from the catalog.
+        course_url = "http://unt.catalog.acalog.com/preview_course.php?catoid=" + str(self.catoid) + "&coid=" + str(self.coid)
+        page = urllib2.urlopen(course_url)
+        return BeautifulSoup(page, 'html.parser')  # This will allow us to use the webpage as an object
+
+    def _determine_title(self, page_data):
+        # type: (BeautifulSoup) -> str
+        # The title is in an <h1> tag with id #course_preview_title
+        return page_data.find('h1', attrs={'id': 'course_preview_title'}).string
+
+    def _determine_prefix(self, title):
+        # type: (string) -> str
+        splitstring = title.split(" ")
+        if len(splitstring)>1:
+            return splitstring[0]
+        else:
+            return str(title)[:4]
+
+    def _determine_number(self, title):
+        # type: (string) -> str
+        splitstring = title.split(" ")
+        if len(splitstring)>1:
+            return splitstring[1]
+        else:
+            return str(title)[4:]
+
+    def is_identical_to_another_course(self,compound=None,prefix=None,number=None):
+        if compound is not None:
+            are_prefixes_identical = (self._determine_prefix(compound) == self.prefix)
+            are_numbers_identical = (self._determine_number(compound) == self.number)
+        else:
+            are_prefixes_identical = (prefix == self.prefix)
+            are_numbers_identical = (number == self.number)
+        return are_numbers_identical and are_prefixes_identical
+    def add_course_to_list(self, prereqcatoid, prereqcoid, list):
+        if prereqcoid in alreadyGeneratedCourses:
+            list.append(alreadyGeneratedCourses[prereqcoid])
+        else:
+            list.append(Course(prereqcatoid, prereqcoid))
+    def find_catoid_from_href(self,href):
+        parsed_url = urlparse.urlparse(href)
+        return urlparse.parse_qs(parsed_url.query)['catoid'][0]
+    def find_coid_from_href(self,href):
+        parsed_url = urlparse.urlparse(href)
+        return urlparse.parse_qs(parsed_url.query)['coid'][0]
+
+    def _determine_prereqs(self, page_data):
+        # the links to the prereqs are <a> elements that are children of .block_content_popup class elements that are
+        # after the text "Prerequisite(s):"
+        links = page_data.find('td', attrs={'class': 'block_content_popup'}).findChildren('a', recursive=False)
+        courses_that_are_pre_reqs = []
+        for link in links:
+            if "Prerequisite(s):" in link.previous_sibling:
+                if link.previous_sibling == "Prerequisite(s): ":
+                    # only add this to the prereqs list if it really is a prereq
+                    # parsedUrlToPreReq = urlparse.urlparse(link['href'])
+                    # preReqCatoid = urlparse.parse_qs(parsedUrlToPreReq.query)['catoid'][0]
+                    # preReqCoid = urlparse.parse_qs(parsedUrlToPreReq.query)['coid'][0]
+
+                    self.add_course_to_list(self.find_catoid_from_href(link['href']), self.find_coid_from_href(link['href']), courses_that_are_pre_reqs)
+                    # This next part is really hacked together. We'll iterate over the next sibling elements until we
+                    # hit a <br>. If it is a link, then we'll add it to the prereq list.
+                    nextelementtocheck = link.find_next_sibling(['br', 'a'])
+                    while True:
+                        if nextelementtocheck.name == u'br':
+                            break
+                        else:
+                            self.add_course_to_list(self.find_catoid_from_href(nextelementtocheck['href']), self.find_coid_from_href(nextelementtocheck['href']), courses_that_are_pre_reqs)
+                            nextelementtocheck = nextelementtocheck.find_next_sibling(['br', 'a'])
+
+                else:
+                    print "ODD PREREQ DETECTED WITH CATOID: " + self.catoid + " COID: " + self.coid
+        return courses_that_are_pre_reqs
+
+    def _determine_coreqs(self, page_data):
+        links = page_data.find('td', attrs={'class': 'block_content_popup'}).findChildren('a', recursive=False)
+        courses_that_are_co_reqs = []
+        for link in links:
+            if "Corequisite(s):" in link.previous_sibling:
+                if link.previous_sibling == "Corequisite(s): ":
+                    # only add this to the prereqs list if it really is a prereq
+                    parsed_url_to_co_req = urlparse.urlparse(link['href'])
+                    co_req_catoid = urlparse.parse_qs(parsed_url_to_co_req.query)['catoid'][0]
+                    co_req_coid = urlparse.parse_qs(parsed_url_to_co_req.query)['coid'][0]
+                    if co_req_coid in alreadyGeneratedCourses:
+                        courses_that_are_co_reqs.append(alreadyGeneratedCourses[co_req_coid])
+                    else:
+                        courses_that_are_co_reqs.append(Course(co_req_catoid, co_req_coid))
+                else:
+                    print "ODD PREREQ DETECTED WITH CATOID: " + self.catoid + " COID: " + self.coid
+        return courses_that_are_co_reqs
+
+    def _determine_hours(self, page_data):
+        credit_hour_text = page_data.find('hr').previous_sibling
+        try:
+            return int(credit_hour_text.split(" hours")[0])
+        except ValueError:
+            return [float(s) for s in re.findall(r'-?\d+\.?\d*', credit_hour_text.split(" hours")[0])][0]
+
+    def printinfo(self, tabs=0, show_prereqs=False,recursive_show_prereqs = False):
+        print '\t' * tabs + self.title
+        print "\t" * tabs + "catoid: " + str(self.catoid)
+        print "\t" * tabs + "coid: " + str(self.coid)
+        print "\t" * tabs + "prefix: " + str(self.prefix)
+        print "\t" * tabs + "number: " + str(self.number)
+        print "\t" * tabs + "hours: " + str(self.hours)
+        print "\t" * tabs + "prereqs: " + ''.join(str(x.coid) for x in self.prereqs)
+        print "\t" * tabs + "coreqs: " + ''.join(str(x.coid) for x in self.coreqs)
+        if len(self.prereqs) > 0 and show_prereqs:
+            print '\t' * tabs + "Prerequisite(s):"
+            for course in self.prereqs:
+                # print course
+                course.printinfo(tabs + 1, show_prereqs=recursive_show_prereqs, recursive_show_prereqs=recursive_show_prereqs)
+        # print "\n"
+
+    def find_coid_from_prefix_and_number(self, catoid, prefix, number):
+        '''search_url = "http://catalog.unt.edu/content.php?filter[27]=" + str(prefix) + "&filter[29]=" + str(
+            number) + "&cur_cat_oid=" + str(catoid) + "&navoid=1723"
+            '''
+        search_url = "http://catalog.unt.edu/search_advanced.php?cur_cat_oid="+str(catoid)+"&filter[keyword]="+str(prefix)+str("+")+str(number)+"&filter[exact_match]=1&filter[3]=1"
+        # print search_url
+        page = urllib2.urlopen(search_url)
+        soup = BeautifulSoup(page, 'html.parser')
+        links_to_courses = soup.select('a[href^="preview_course_nopop.php"]')
+        # print links_to_courses
+        href_to_class = None
+        if len(links_to_courses): # check if it exists, so that we don't throw an error
+            href_to_class = links_to_courses[0]['href']
+            parsed_url = urlparse.urlparse(href_to_class)
+            parsed_queries = urlparse.parse_qs(parsed_url.query)
+            found_coid = None
+            # print parsed_queries
+            if u'coid' in parsed_queries:
+                found_coid = parsed_queries['coid'][0] # This checks the url for whatever is in the coid spot.
+            else:
+                # For some forsaken reason, there is an extra, blank link on some pages. I can't find a pattern to it
+                # right now.
+                if len(links_to_courses)>1:
+                    href_to_class = links_to_courses[1]['href']
+                    parsed_url = urlparse.urlparse(href_to_class)
+                    parsed_queries = urlparse.parse_qs(parsed_url.query)
+                    # print "New parsed queries: " + str(parsed_queries)
+                    found_coid = parsed_queries['coid'][0]
+                else:
+                    found_coid = None
+                    # raise Exception("We couldn't find a coid?")
+            return found_coid
+        else:
+            print('Course does not seem to exist in that search')
+            return None
+
+
+    def __init__(self, catoid, coid=None, prefix = None, number = None, compound = None):
+        if coid is None and compound is None:
+            self.coid = self.find_coid_from_prefix_and_number(catoid, prefix, number)
+        elif coid is None and compound is not None:
+            self.coid = self.find_coid_from_prefix_and_number(catoid, str(compound)[:4], str(compound)[4:])
+        else:
+            self.coid = coid
+
+        self.catoid = catoid
+
+        # Add this instance to the alreadyGeneratedCourses List at spot [coid]
+        alreadyGeneratedCourses[coid] = self
+        if self.coid is not None:
+            pageData = self._get_dom_from_webpage()  # Scrape the page data from the webpage
+            self.title = self._determine_title(pageData)
+            self.prefix = self._determine_prefix(self.title)
+            self.number = self._determine_number(self.title)
+            # self.prereqs = self._determine_prereqs(pageData)
+            # self.coreqs = self._determine_coreqs(pageData)
+            self.hours = self._determine_hours(pageData)
+        else:
+            if compound is not None:
+                self.prefix = self._determine_prefix(compound)
+                self.number = self._determine_number(compound)
+            if compound is None:
+                if prefix is not None:
+                    self.prefix = prefix
+                if number is not None:
+                    self.number = number
+            self.title = "Unknown Title"
+            self.hours = "?"
+
+# Class Semester
+# The Semester class has properties including year and term
+# method "get_hours" which calculates and returns how many hours of classes are taken that semester
+# method "get_classes" returns the set of classes
+class Semester:
+    classes = {}  # type: Course
+
+    def __init__(self, classes = None, year = None, term = None, compound_semester_id = None):
+        if classes:
+            self.classes = classes
+
+        if year and term:
+            self.year = year
+            self.term = term
+        elif compound_semester_id and year is None and term is None:
+            split_str = str(compound_semester_id).split(".")
+            self.year = split_str[0]
+            self.term = split_str[1]
+        else:
+            raise Exception('Invalid semester initialization.')
+
+    def get_classes(self):
+        return self.classes
+
+    def add_class(self, course):
+        self.classes.add(course)
+
+    def get_hours(self):
+        running_total_of_hours = 0
+        for course in self.classes:
+            running_total_of_hours += course.hours
+        return running_total_of_hours
+
+    def print_semester(self):
+        print "Semester: " + str(self.year)+"."+str(self.term)
+        for course in self.classes:
+            course.printinfo(tabs=1)
+            print "\n"
+
+class FourYearPlan:
+    semesters = {}  # type: Semester
+
+    def __init__(self, semesters={}):
+        self.semesters = semesters
+
+    def add_semester(self, semester):
+        self.semesters[float(str(semester.year) + "." + str(semester.term))] = semester
+
+    def check_semester_for_prereqs(self, semesterIndex):
+        # loop over each course in semester with index semesterIndex
+        # record the pre req courses into a dictionary with value False, meaning that it has not yet been marked as taken
+        # check each semester, if it's earlier, then check each class.
+        # If said class matches on in the above dictionary, change value to True
+        # If there is a class that is still False at the end, return an error message saying that the class is missing a pre req
+
+        # make a composite list of all previous classes
+        all_previous_classes = {}
+        for individualIndex in self.semesters:
+            if individualIndex < semesterIndex:
+                for previous_course in self.semesters[individualIndex].classes:
+                    all_previous_classes.append(previous_course)
+
+        for course in self.semesters[semesterIndex].classes:
+            required_prereqs = {}
+            for prereq in course.prereqs:
+                required_prereqs[prereq.coid] = False
+            for previous_course in all_previous_classes:
+                if previous_course == course:
+                    required_prereqs[course.coid] = True
+            print required_prereqs
+            # if False in required_prereqs.values():
+            #    raise Exception('spam', 'egg')
+            # TODO: Implement some way of notifying the program if prereqs aren't satisfied.
+
+        '''
+        for course in self.semesters[semesterIndex].classes:
+            required_prereqs = {}
+            for prereq in course.prereqs:
+                required_prereqs[prereq.coid] = False
+            for individualIndex in self.semesters:
+                if individualIndex < semesterIndex:
+                    for previous_course in self.semesters[individualIndex].classes:
+                        if previous_course == course:
+                            required_prereqs[course.coid] = True
+        '''
+
+    # function import_audit
+    # inputs: str audit_uri
+    # outputs: void
+    # import_audit takes a URI to an audit html file, then turns all the already completed and in progress classes into
+    # Course objects in the parent FourYearPlan object.
+    def import_audit(self, audit_uri):
+        soup = BeautifulSoup(open(audit_uri), 'html.parser')
+        audit_catoid = None
+        for td in soup.find(attrs={"class":"auditHeaderTable"}).findAll("td"):
+            if td.previous_sibling.string == "Catalog Year":
+                catalog_string = td.string
+                catalog_year = int(catalog_string.split(" ")[1])
+                audit_catoid = 2*catalog_year - 4017 # This formula converts the catalog year into the catoid. It's
+                # funky because the catoids started in 2009. Graduate catalogs are even numbers (staritng with 0) and
+                # undergrads are odd numbers (starting with 1)
+        for blarg in soup.findAll("tr", attrs={"class", "takenCourse"}):
+            semester_id = blarg.find(attrs={"class": "term"}).string
+            course_compound_id = blarg.find(attrs={"class": "course"}).string
+            if float(semester_id) in self.semesters:
+                # add course to semester
+                self.semesters[float(semester_id)].add_class(Course(catoid=audit_catoid, compound=course_compound_id))
+            else:
+                # add semester then add course
+                temp_semester_variable = Semester(classes={Course(catoid=audit_catoid, compound=course_compound_id)},
+                                           compound_semester_id=semester_id)
+                self.add_semester(temp_semester_variable)
+
+
+
+# testSemester = Semester({Course(u'17', u'63011'), Course(u'17', u'63942'), Course(u'17', u'63929')}, 17, 0)
+# testPlan = FourYearPlan()
+
+
+
+# #####
+# Importer notes
+# Test document: file:///Users/andrewsansom/Downloads/My%20Audit%20-%20Audit%20Results%20Tab.html
+# All courses that could be taken to fulfill something in the audit has the css selector span.course
+# All courses that have been taken are "tr.takenCourse:not(.ip)"
+# All courses that are in progress are "tr.takenCourse.ip"
+# #####
+
+
+
+
+
+def main():
+    # print "started"
+    #testCourse = Course(u'17', u'63011')
+    #testCourse.printinfo(show_prereqs=True)
+
+    # print alreadyGeneratedCourses
+    # for course in alreadyGeneratedCourses:
+    #    alreadyGeneratedCourses[course].printinfo()
+
+    # testPlan.add_semester(testSemester)
+    # print testSemester.get_classes()
+    # print testSemester.get_hours()
+
+    # testPlan.check_semester_for_prereqs(17.0)
+    # print testPlan.semesters[17.0]
+    ''''''
+
+if __name__ == "__main__":
+    main()
+
+
---
+++ venv/tests.py
@@ -0,0 +1,150 @@
+import unittest
+from main import *
+import StringIO
+import sys
+import string
+
+converted_test_audit = {
+    14.5: Semester({Course(catoid=u'17', compound="PSCI2G05")}, 14, 5),
+    15.8: Semester({Course(catoid=u'17', compound="ENGL2210"),
+                    Course(catoid=u'17', compound="ENGL2220"),
+                    Course(catoid=u'17', compound="HIST1050"),
+                    Course(catoid=u'17', compound="HIST1060"),
+                    Course(catoid=u'17', compound="HIST1N01"),
+                    Course(catoid=u'17', compound="HNRS1500"),
+                    Course(catoid=u'17', compound="MATH1710"),
+                    Course(catoid=u'17', compound="MATH1710"),
+                    Course(catoid=u'17', compound="MATH1720"),
+                    Course(catoid=u'17', compound="MATH2700"),
+                    Course(catoid=u'17', compound="MATH2730"),
+                    Course(catoid=u'17', compound="MUAS1506"),
+                    Course(catoid=u'17', compound="MUTH1400"),
+                    Course(catoid=u'17', compound="MUTH1500"),
+                    Course(catoid=u'17', compound="PHYS1270"),
+                    Course(catoid=u'17', compound="PHYS1410"),
+                    Course(catoid=u'17', compound="PHYS1420"),
+                    Course(catoid=u'17', compound="PHYS1430"),
+                    Course(catoid=u'17', compound="PHYS1440"),
+                    Course(catoid=u'17', compound="SPAN1010"),
+                    Course(catoid=u'17', compound="SPAN1020"),
+                    Course(catoid=u'17', compound="SPAN2040"),
+                    Course(catoid=u'17', compound="SPAN2050"),
+                    Course(catoid=u'17', compound="TNTX1100")
+                    }, 15, 8),
+    16.1: Semester({Course(catoid=u'17', compound="MATH2100"),
+                    Course(catoid=u'17', compound="MATH3000"),
+                    Course(catoid=u'17', compound="MUTH2400"),
+                    Course(catoid=u'17', compound="PHYS2220"),
+                    Course(catoid=u'17', compound="PHYS2240"),
+                    Course(catoid=u'17', compound="PSCI1041"),
+                    Course(catoid=u'17', compound="TNTX1200")
+                    }, 16, 1),
+    16.8: Semester({Course(catoid=u'17', compound="CSCE1030"),
+                    Course(catoid=u'17', compound="ECON1110"),
+                    Course(catoid=u'17', compound="MATH3610"),
+                    Course(catoid=u'17', compound="MATH3680"),
+                    Course(catoid=u'17', compound="PHYS3010"),
+                    Course(catoid=u'17', compound="PHYS3030")
+                    }, 16, 8),
+    19.1: Semester({Course(catoid=u'17', compound="CSCE1040"),
+                    Course(catoid=u'17', compound="EDCI3500"),
+                    Course(catoid=u'17', compound="ENGL1310"),
+                    Course(catoid=u'17', compound="MATH2000"),
+                    Course(catoid=u'17', compound="MATH3510")
+                    }, 19, 1)
+}  # type: Set[Semester]
+
+
+class TestCourseMethods(unittest.TestCase):
+    def setUp(self):
+        self.testCourse = Course(u'17', u'63011')
+        self.page_data = self.testCourse._get_dom_from_webpage()
+
+    def test_get_dom_from_webpage(self):
+        # self.testCourse._get_dom_from_webpage()
+        pass
+
+    def test_determine_title(self):
+        self.assertEquals(self.testCourse._determine_title(self.page_data),
+                          "CSCE 4240 - Introduction to Digital Image Processing")
+
+    def test_compound_init(self):
+        self.testCourse = Course(u'13', compound="PSCI1041")
+        self.assertEquals(self.testCourse.coid, u'43946')
+        self.assertEquals(self.testCourse.prefix, u'PSCI')
+        self.assertEquals(self.testCourse.number, u'1041')
+
+    def test_is_identical_to_another_course(self):
+        self.assertTrue(self.testCourse.is_identical_to_another_course(compound="CSCE4240"))
+
+    def test_nonexistant_class(self):
+        self.testCourse = Course(u'17', compound="PSCI2G05")
+
+
+class TestSemesterMethods(unittest.TestCase):
+    def setUp(self):
+        self.testSemester = Semester({Course(catoid=u'17', compound="CSCE1040"),
+                                      Course(catoid=u'17', compound="EDCI3500")
+                                      }, 19, 1)
+
+    def test_print_semester(self):
+        # We have to do some tricky manipulation to test a print.
+        test_output = '''Semester: 19.1
+	CSCE 1040 - Computer Science II
+	catoid: 17
+	coid: 62976
+	prefix: CSCE
+	number: 1040
+	hours: 3
+	prereqs: 
+	coreqs:
+	EDCI 3500 - Knowing and Learning in Mathematics, Science and Computer Science
+	catoid: 17
+	coid: 63227
+	prefix: EDCI
+	number: 3500
+	hours: 3
+	prereqs: 
+	coreqs: 
+'''
+        captured_output = StringIO.StringIO()
+        sys.stdout = captured_output
+        self.testSemester.print_semester()
+        sys.stdout = sys.__stdout__
+        # self.assertEquals(captured_output.getvalue().encode('utf-8').translate(None,string.whitespace),
+        #                   test_output.translate(None,string.whitespace))
+        # This old test is bad, because sometimes the order can change. Instead we'll just look for certain keywords.
+        self.assertIn('CSCE 1040 - Computer Science II', captured_output.getvalue())
+        self.assertIn('EDCI 3500 - Knowing and Learning in Mathematics, Science and Computer Science',
+                      captured_output.getvalue())
+
+
+class TestFourYearPlanMethods(unittest.TestCase):
+    def setUp(self):
+        self.testFourYearPlan = FourYearPlan()
+
+    def test_import_audit(self):
+        audit_uri = "testAudit.html"
+
+        self.testFourYearPlan.import_audit(audit_uri)
+        print self.testFourYearPlan.semesters, converted_test_audit
+        # self.assertEqual(self.testFourYearPlan.semesters, converted_test_audit)
+
+        for semester in converted_test_audit:
+            if semester in self.testFourYearPlan.semesters:
+                check_semester = converted_test_audit[semester]
+                test_semester = self.testFourYearPlan.semesters[semester]
+                # check_semester.print_semester()
+                for course in check_semester.classes:
+                    # see if course (or an identical one) is in test_semester.classes
+                    # check over each course in test_semester.classes. if they have the same id then there is a match.
+                    # if there is no match, then throw a failure
+                    self.assertTrue(any(
+                        (test_class.is_identical_to_another_course(prefix=course.prefix, number=course.number)) for
+                        test_class in test_semester.classes))
+            else:
+                fail("Missing semester")
+
+
+if __name__ == '__main__':
+    unittest.main()
